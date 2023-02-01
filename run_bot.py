385784b66f901e35896e0a6c556e13bea45ad52f
Charles Frye <cfrye59@gmail.com>
2023-02-01 13:10:40 -0800

    switches over to slash commands for bot, closes #8

--- run_bot.py
+++ run_bot.py
@@ -3,6 +3,7 @@ import argparse
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import requests
 
@@ -11,7 +12,6 @@ load_dotenv()
 MODAL_USER_NAME = os.environ["MODAL_USER_NAME"]
 BACKEND_URL = f"https://{MODAL_USER_NAME}--ask-fsdl-hook.modal.run"
 DISCORD_AUTH = os.environ["DISCORD_AUTH"]
-BASE_TRIGGER_PHRASE = "$ask-fsdl"
 
 
 def runner(query):
@@ -23,41 +23,39 @@ def runner(query):
     return response.json()["answer"]
 
 
-def main(targeted_channels, trigger_phrase, auth):
+
+def main(targeted_channels, auth):
     # Discord auth requires statement of "intents"
     #  we start with default behaviors
     intents = discord.Intents.default()
     #  and add reading messages
     intents.message_content = True
-    # then connect to Discord
-    client = discord.Client(intents=intents)
 
+    # create the base bot
+    bot = commands.Bot(intents=intents)
 
-    # define the bot's behavior
-    @client.event
+    @bot.event
     async def on_ready():
-        print(f'We have logged in as {client.user}')
+        print(f"🤖: {bot.user} is ready and online!")
 
-
-    @client.event
-    async def on_message(message):
-        if message.channel.id not in targeted_channels:
+    # add our command
+    @bot.slash_command(name="ask", description="Answers questions about FSDL material.")
+    async def answer(ctx, question: str):
+        """Answers questions about FSDL material."""
+        if ctx.channel.id not in targeted_channels:
             return
 
-        if message.author == client.user:
+        if ctx.author == bot.user:
             # ignore posts by self
             return
         else:
-            respondent = message.author
+            respondent = ctx.author
 
-        if message.content.startswith(trigger_phrase):
-            header, *content = message.content.split(trigger_phrase)  # parse
-            content =  "".join(content).strip()
-            print(f"🤖: responding to message \"{content}\"")
-            response = runner(content)  # execute
-            await message.channel.send(f'{respondent.mention} {response}')  # respond
+        print(f"🤖: responding to question \"{question}\"")
+        response = runner(question)  # execute
+        await ctx.send(f"{respondent.mention} asked: {question}\n\nHere's my response, with sources so you can read more:\n\n{response}")  # respond
 
-    client.run(auth)
+    bot.run(auth)
 
 
 def make_argparser():
@@ -74,13 +72,11 @@ if __name__ == "__main__":
     targeted_channels = [
         1066557596313604200, # main channel: `ask-fsdl`
     ]
-    trigger_phrase = BASE_TRIGGER_PHRASE
 
     if args.dev:
         targeted_channels = [
         1066450466898186382, # dev channel: `ask-fsdl-dev`
         984528990368825395,  # `instructor-lounge`
         ]
-        trigger_phrase = "$dev-" + BASE_TRIGGER_PHRASE.strip("$")
 
-    main(targeted_channels, trigger_phrase, auth=DISCORD_AUTH)
+    main(targeted_channels, auth=DISCORD_AUTH)
