e9b62ba83f400c4b291a25d190d8c63fd97b5ff8
Charles Frye <cfrye59@gmail.com>
2023-02-01 18:20:07 -0800

    brings back dev mode, adds debugging

--- run_bot.py
+++ run_bot.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 
+import aiohttp
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -13,25 +14,29 @@ MODAL_USER_NAME = os.environ["MODAL_USER_NAME"]
 BACKEND_URL = f"https://{MODAL_USER_NAME}--ask-fsdl-hook.modal.run"
 DISCORD_AUTH = os.environ["DISCORD_AUTH"]
 
+guild_ids = {
+    "dev": 1070516629328363591,
+    "prod": 984525101678612540,
+}
 
-def runner(query):
+
+async def runner(query):
     payload = {"query": query}
-    response = requests.get(url=BACKEND_URL, params=payload)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=BACKEND_URL, params=payload) as response:
+            assert response.status == 200
+            json = await response.json()
+            return json["answer"]
 
-    response.raise_for_status()
 
-    return response.json()["answer"]
-
-
-def main(auth):
+def main(auth, guilds, debug=False):
     # Discord auth requires statement of "intents"
     #  we start with default behaviors
     intents = discord.Intents.default()
     #  and add reading messages
     intents.message_content = True
 
-    # create the base bot
-    bot = commands.Bot(intents=intents)
+    bot = commands.Bot(intents=intents, guilds=guilds)
 
     @bot.event
     async def on_ready():
@@ -43,6 +48,7 @@ def main(auth):
     Here's my best guess at an answer, with sources so you can read more:
 
     {response}"""
+
     # add our command
     @bot.slash_command(name="ask", description="Answers questions about FSDL material.")
     async def answer(ctx, question: str):
@@ -54,15 +60,29 @@ def main(auth):
         response = runner(question)  # execute
         await ctx.send_followup(response_fmt.format(mention=respondent.mention, question=question, response=response))  # respond
 
+    if debug:
+        @bot.slash_command()
+        async def health(ctx):
+            "Supports a Discord bot version of a liveness probe."
+            print(f"🤖: inside foo")
+            await ctx.respond("200 more like 💯 mirite")
+
     bot.run(auth)
 
 
 def make_argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dev", action="store_true", help="Run in development mode.")
+    parser.add_argument("--debug", action="store_true", help="Add debugging commands.")
+    parser.add_argument("--monitor", action="store_true", help="Log bot behavior.")
 
     return parser
 
 
 if __name__ == "__main__":
-    main(auth=DISCORD_AUTH)
+    args = make_argparser().parse_args()
+    if args.dev:
+        guilds = [guild_ids["dev"]]
+    else:
+        guilds = [guild_ids["prod"]]
+    main(auth=DISCORD_AUTH, guilds=guilds, debug=args.dev or args.debug)
