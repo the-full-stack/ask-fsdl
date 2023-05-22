8c654f71db80c03bdccbe72637f5470a605c3244
Charles Frye <cfrye59@gmail.com>
2023-05-22 15:20:52 -0700

    adds full drop of docstore

--- docstore.py
+++ docstore.py
@@ -13,10 +13,20 @@ def get_documents(client, db="fsdl", collection=MONGO_COLLECTION):
 def connect():
     """Connects to a document database, here MongoDB."""
     import os
+
     import pymongo
 
     mongodb_password = os.environ["MONGODB_PASSWORD"]
     mongodb_uri = os.environ["MONGODB_URI"]
     connection_string = f"mongodb+srv://fsdl:{mongodb_password}@{mongodb_uri}/?retryWrites=true&w=majority"
     client = pymongo.MongoClient(connection_string)
+
     return client
+
+
+def flush():
+    client = connect()
+
+    db = client.get_database("fsdl")
+    collection = db.get_collection(MONGO_COLLECTION)
+    collection.drop()
