697171396e041301970d9fa41237fb7853067ebf
Charles Frye <cfrye59@gmail.com>
2023-05-30 11:53:24 -0700

    better control of pulumi stack, simpler access rules

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,103 +1,91 @@
 """Resource definition for the bot application."""
+# TODO: AWS secrets/config setup
+from pathlib import Path
+
 import pulumi
 import pulumi_aws as aws
 
-# TODO: make target for pulumi
-# TODO: AWS secrets/config setup
+# Retrieve the current stack name
+STACK_NAME = pulumi.get_stack()
 
-GIT_REPO = "https://github.com/the-full-stack/ask-fsdl.git"
+# Use the stack name to conditionally change program behavior
+if STACK_NAME == "dev":
+    pulumi.log.info("Running in the 'dev' stack.")
+    env_file = Path("..") / ".env.dev"
+else:
+    if STACK_NAME != "prod":
+        pulumi.log.warning("Unknown stack name, defaulting to prod.")
+    pulumi.log.info("Running in the 'prod' stack.")
+    env_file = Path("..") / ".env"
 
-# TODO: wait, can I just configure the security group and call it a day?
-# Create a new VPC
-vpc = aws.ec2.Vpc("ask-fsdl-vpc", cidr_block="10.0.0.0/16")
+# Get the Pulumi config object
+cfg = pulumi.Config()
+cfg.require("MODAL_USER_NAME")
+cfg.require_secret("DISCORD_AUTH")
 
-# Create a new public subnet
-subnet = aws.ec2.Subnet(
-    "ask-fsdl-subnet",
-    vpc_id=vpc.id,
-    cidr_block="10.0.0.0/24",
-    availability_zone="us-west-2a",
+GIT_ORG = "the-full-stack"
+GIT_REPO = "ask-fsdl"
+GIT_BRANCH = "main" if STACK_NAME == "prod" else "charles/pulumi"
+GIT_URI = f"https://github.com/{GIT_ORG}/{GIT_REPO}.git"
+
+# allow inbound SSH access
+ssh_ingress_rule = aws.ec2.SecurityGroupIngressArgs(
+    protocol="tcp",
+    from_port=22,  # Allow SSH access
+    to_port=22,
+    cidr_blocks=["0.0.0.0/0"],  # from all IPs
 )
 
-# Create an internet gateway
-gateway = aws.ec2.InternetGateway("ask-fsdl-internet-gateway", vpc_id=vpc.id)
-
-# Create a route table
-route_table = aws.ec2.RouteTable(
-    "ask-fsdl-route-table",
-    vpc_id=vpc.id,
-    routes=[
-        aws.ec2.RouteTableRouteArgs(
-            cidr_block="0.0.0.0/0",
-            gateway_id=gateway.id,
-        ),
-    ],
+# open outbound traffic
+open_egress_rule = aws.ec2.SecurityGroupEgressArgs(
+    protocol="-1",  # Allow all protocols
+    from_port=0,  # on all ports
+    to_port=0,
+    cidr_blocks=["0.0.0.0/0"],  # to all IPs
 )
 
-# Associate the route table with the subnet
-association = aws.ec2.RouteTableAssociation(
-    "ask-fsdl-route-table-association",
-    subnet_id=subnet.id,
-    route_table_id=route_table.id,
-)
-
-
-# allow SSH access
 security_group = aws.ec2.SecurityGroup(
-    "ask-fsdl-ssh-access",
-    vpc_id=vpc.id,
-    ingress=[
-        aws.ec2.SecurityGroupIngressArgs(
-            protocol="tcp",
-            from_port=22,
-            to_port=22,
-            cidr_blocks=["0.0.0.0/0"],  # Allow SSH access from all IPs
-        ),
-    ],
-    egress=[
-        aws.ec2.SecurityGroupEgressArgs(
-            protocol="-1",  # Allow all protocols
-            from_port=0,
-            to_port=0,
-            cidr_blocks=["0.0.0.0/0"],  # Allow all outbound traffic
-        ),
-    ],
+    "bot-sg",
+    ingress=[ssh_ingress_rule],
+    egress=[open_egress_rule],
 )
 
-# TODO: handle secrets -- scp from local?
+
+def config_as_env():
+    """Return the config elements as environment variables, .env style."""
+    # these will be plaintext in the internal Pulumi state.
+    # for greater security we could use Pulumi's secret management.
+    sensitive_keys = ["DISCORD_AUTH", "DISCORD_AUTH_DEV", "DISCORD_MAINTAINER_ID"]
+    keys = ["MODAL_USER_NAME"] + sensitive_keys
+    return "\n".join([f"{key}={cfg.get(key)}" for key in keys])
+
+
+def build_startup_script():
+    # write the startup script for the EC2 instance
+    script = "#!/bin/bash\n"
+    script += "sudo yum update -y\n"  # update yum, package manager for amazon linux
+    script += "sudo yum install -y git\n"  # install git for cloning the repo
+    script += "sudo amazon-linux-extras install -y python3.8\n"  # install recent python
+    script += "git clone -b {GIT_BRANCH} {GIT_URI}\n"  # clone the repo
+    script += "cd ask-fsdl\n"  # change directory into the repo
+    script += f"""echo "{config_as_env()}" >> .env\n"""  # write the config file
+    script += "python3.8 -m pip install -r requirements.txt\n"  # install dependencies
+    script += "python3.8 bot/run.py"  # run the Discord bot
+    script += "--dev\n" if STACK_NAME == "dev" else "\n"  # in dev mode if in dev stack
+
+    return script
+
+
 # Create an AWS EC2 instance
 instance = aws.ec2.Instance(
-    "ask-fsdl-bot",
+    "bot-server",
     instance_type="t2.micro",
     ami="ami-0ceecbb0f30a902a6",
-    subnet_id=subnet.id,
-    key_name="fsdl-webserver-keys",  # TODO: handle keygen
-    # TODO: is there a better AMI for this? maybe with Ubuntu?
-    user_data=f"""#!/bin/bash
-    sudo yum update -y
-    sudo yum install -y git
-    sudo amazon-linux-extras install -y python3.8
-    git clone {GIT_REPO}
-    cd ask-fsdl
-    python3.8 -m pip install -r requirements.txt
-    python3.8 bot/run_bot.py --dev
-    """,
+    key_name="fsdl-webserver-keys",  # TODO: handle keygen with aws.ec2.KeyPair
+    user_data=build_startup_script(),
+    user_data_replace_on_change=True,  # reprovision if user_data changes
     vpc_security_group_ids=[security_group.id],
-)
-# TODO: try this:
-# alias python='python3.8'
-# and go back to using the make commands
-
-# Create a public IP address for the instance
-eip = aws.ec2.Eip("ask-fsdl-instance-eip", instance=instance.id, vpc=True)
-
-# Create a route to the internet gateway
-route = aws.ec2.Route(
-    "ask-fsdl-route",
-    route_table_id=route_table.id,
-    destination_cidr_block="0.0.0.0/0",
-    gateway_id=gateway.id,
 )
 
 # Export the public IP of the EC2 instance for convenience
-pulumi.export("public_ip", instance.public_ip)
+pulumi.export("bot-public-ip", instance.public_ip)
