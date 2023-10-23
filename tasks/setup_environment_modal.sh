e598073dcc36f1054d3b2e1d86306b436f520c2b
Charles Frye <cfrye59@gmail.com>
2023-10-22 20:09:04 -0700

    workaround for failure in notebook

--- tasks/setup_environment_modal.sh
+++ tasks/setup_environment_modal.sh
@@ -2,13 +2,13 @@
 # shellcheck source=tasks/pretty_log.sh
 set -euo pipefail
 
-env=${1:-"dev"}
+export env=${1:-"dev"}
 
 # clear command-line parameters
 set --
 source tasks/pretty_log.sh
 
-if modal environment list --json | grep -q "\"name\": \"$env\""; then
+if modal environment list --json | grep -q "\"name\".*\"$env\""; then
   pretty_log "Found modal environment $env"
 else
   pretty_log "Setting up modal environment $env"
